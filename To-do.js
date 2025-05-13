e02ed3d330ad9ec363f36ab315ecf5ab6401e694
Alphinjosho <153099931+Alphinjosho@users.noreply.github.com>
2025-05-14 00:53:34 +0530

    Upadate To do project features
    .Add tasks to Unfinished Task area .Enable scrolling  in unfinished Task section .Handel large task content without breaking layout

--- To-do.js
+++ To-do.js
@@ -1,21 +1,36 @@
     let add = document.getElementById("add");
     let error= document.getElementById("Perror")
 
-
-
-    // add.addEventListener("",(e)=>{
-    //     e.preventDefault();
-    //     formValidation();
-    // })
-
    let formValidation=()=>{
      if (add.value.trim()===""){
      error.style.display="block"
    } else{
      error.style.display="none"
+     addTask();
    }
   }
 
   //  Function for UNfinshied Tasks
-  
-    
+   function addTask (){
+     let div=document.createElement("div");
+     let checkbox=document.createElement("input");
+     checkbox.type="checkbox";
+     let label= document.createElement("label");
+     label.textContent= document.getElementById("add").value;
+     let span = document.createElement("span");
+     span.className = "options";
+     let editIcon = document.createElement("i");
+     editIcon.className ="fa-solid fa-pen-to-square";
+     let deleteIcon = document.createElement("i");
+     deleteIcon.className ="fa-solid fa-trash";
+      div.appendChild(checkbox);
+      div.appendChild(label);
+      span.appendChild(editIcon);
+      span.appendChild(deleteIcon);
+      div.appendChild(span);
+      
+    document.getElementById("Tasks").appendChild(div);
+    document.getElementById("add").value = "";
+
+    }
+      
