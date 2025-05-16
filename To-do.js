0c24499554dc1770c1ff08830ebebbd7a6109d8e
Alphinjosho <153099931+Alphinjosho@users.noreply.github.com>
2025-05-16 23:35:05 +0530

    Added edit task functionality

--- To-do.js
+++ To-do.js
@@ -1,5 +1,6 @@
     let add = document.getElementById("add");
     let error= document.getElementById("Perror")
+    let taskbeingEdited=null;
 
    let formValidation=()=>{
      if (add.value.trim()===""){
@@ -11,18 +12,34 @@
   }
 
   //  Function for UNfinshied Tasks
+  //  Editing of Task  is also include 
+  
    function addTask (){
+let inputValue = add.value;
+       if (taskbeingEdited !==null){
+        taskbeingEdited.textContent=inputValue
+        taskbeingEdited=null;
+        document.getElementById("add").value="";
+       }
+       else{
      let div=document.createElement("div");
+
      let checkbox=document.createElement("input");
      checkbox.type="checkbox";
+
      let label= document.createElement("label");
-     label.textContent= document.getElementById("add").value;
+     label.textContent= inputValue
+
      let span = document.createElement("span");
      span.className = "options";
+     
      let editIcon = document.createElement("i");
      editIcon.className ="fa-solid fa-pen-to-square";
+     editIcon.addEventListener("click", editTask);
+
      let deleteIcon = document.createElement("i");
      deleteIcon.className ="fa-solid fa-trash";
+
       div.appendChild(checkbox);
       div.appendChild(label);
       span.appendChild(editIcon);
@@ -31,6 +48,17 @@
       
     document.getElementById("Tasks").appendChild(div);
     document.getElementById("add").value = "";
+       }
+     
+ } 
+  // Task Edit 
+ function editTask(e){
+       let parentElement = e.target.parentElement.parentElement;
+       let label = parentElement.querySelector("label");
+       let currentText = label.textContent;
+       document.getElementById("add").value=currentText
+       taskbeingEdited = label;
+ }
 
-    }
-      
+ // Task delete 
+ 
